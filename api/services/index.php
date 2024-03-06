74c58585622ecaafb596ee3ce7c83ff59f163172
Alexandreferreiracampos <alexandrerazrmaxx@gmail.com>
2024-03-05 23:29:03 -0300

    metodo de servicos

--- api/services/index.php
+++ api/services/index.php
@@ -1,36 +1,111 @@
 <?php
 
+// Permitir solicitações de qualquer origem
+header("Access-Control-Allow-Origin: *");
+
+// Permitir métodos de solicitação GET, POST e OPTIONS
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+
+// Permitir os cabeçalhos de solicitação Authorization e Content-Type
+header("Access-Control-Allow-Headers: Authorization, Content-Type");
+
+// Permitir credenciais de usuário em solicitações
+header("Access-Control-Allow-Credentials: true");
+
+// Definir o tipo de conteúdo para aplicativo/json
+header("Content-Type: application/json");
+
 require_once("../../class/database.class.php");
+require_once("../../function/validar-token.php");
+
 
 $con = new Database();
 $link = $con->getConexao();
+$linkId = $con->getConexao();
 
-    $query = "SELECT * FROM services";
-    $stmt = $link->prepare($query);
-    $stmt->execute();
+$token = null;
+$headers = getallheaders();
 
-    if ($stmt->rowCount() > 0) {
-        // Inicializa um array para armazenar os resultados
-        $services = array();
-
-        // Loop através dos resultados e adiciona apenas os índices associativos ao array
-        while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
-            $service = array(
-                'id' => $row['id'],
-                'catSlug' => $row['catslug'],
-                'name' => $row['service_name']
-            );
-            $services[] = $service;
-        }
-
-        // Retorna os dados como JSON
-        header('Content-Type: application/json');
-        echo json_encode($services);
-   
-} else {
-    echo "Não existe dados de serviços";
+if (!isset($_SERVER['HTTP_AUTHORIZATION'])) {
+    http_response_code(401);
+    exit(json_encode(array("message" => "Token de autorização não fornecido")));
 }
 
-$link = null; // Fechar conexão PDO
+$authorizationHeader = $_SERVER['HTTP_AUTHORIZATION'];
+$tokenParts = explode(" ", $authorizationHeader);
+$token = isset($tokenParts[1]) ? $tokenParts[1] : null;
 
-?>
+if (!$token) {
+    http_response_code(401);
+    exit(json_encode(array("message" => "Token de autorização inválido")));
+}
+
+$validarToken = token($headers, $token);
+
+
+if ($validarToken !== null) {
+    // A decodificação foi bem-sucedida, agora você pode usar o objeto
+
+    $id = $validarToken->user_id;
+
+    if (isset($_GET['id'])) {
+
+        $id_empresa = $_GET['id'];
+
+        $query = 'SELECT catservico, MIN(id) AS id FROM servicos WHERE id_empresa = :id_empresa GROUP BY catservico;';
+        $stmt = $link->prepare($query);
+        $stmt->bindParam(':id_empresa', $id_empresa);
+        $stmt->execute();
+
+        if ($stmt->rowCount() > 0) {
+
+            while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+                $serviceEmpresa = array(
+                    'id' => $row['id'],
+                    'catservico' => $row['catservico'],
+                );
+                $empresaArray[] = $serviceEmpresa;
+            }
+
+            header('Content-Type: application/json');
+            echo json_encode($empresaArray);
+        } else {
+            header('Content-Type: application/json');
+            echo json_encode(array("message" => "Empresa não encontrada."));
+        }
+    }
+
+    if (isset($_GET['categoria']) && isset($_GET['id'])) {
+
+        $categoria = $_GET['categoria'];
+        $id_empresa = $_GET['id'];
+
+        $query = 'SELECT * FROM servicos WHERE catservico = :categoria AND id_empresa = :id_empresa';
+        $stmt = $link->prepare($query);
+        $stmt->bindParam(':categoria', $categoria);
+        $stmt->bindParam(':categoria', $id_empresa);
+        $stmt->execute();
+
+        if ($stmt->rowCount() > 0) {
+
+            $row = $stmt->fetch(PDO::FETCH_ASSOC);
+
+            $empresa = array(
+                'id' => $row['id']
+            );
+
+            $empresaArray[] = $empresa;
+            header('Content-Type: application/json');
+            echo json_encode($empresaArray);
+        } else {
+            header('Content-Type: application/json');
+            echo json_encode(array("message" => "Empresa não encontrada."));
+        }
+
+
+
+        echo $categoria;
+
+    }
+
+}
